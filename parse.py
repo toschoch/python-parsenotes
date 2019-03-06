80ca5b43e2e38f11efa042df9c14577897fe3853
Tobias Schoch <Tobias.Schoch@helbling.ch>
2019-03-06 13:14:17 +0100

    initial script and requirements

---
+++ parse.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# created: 05.03.2019
+# author:  TOS
+
+import logging
+import glob
+import cssutils
+import bs4
+from dateutil.parser import parse
+from datetime import datetime
+
+log = logging.getLogger(__name__)
+
+def parsePasswords(content):
+
+    lines = iter(content.splitlines())
+    lastline = None
+    passwords = []
+    while True:
+        try:
+            body = {}
+            if lastline is not None:
+                body['title'] = lastline.strip(':,')
+                lastline = None
+            else:
+                body['title'] = next(lines).strip(':,')
+            if body['title'].lower().startswith('upc') and not body['title'].lower().startswith('upc con'):
+                parts = body['title'].split(' ')
+                body['title'] = parts[0].strip()
+                body['user'] = parts[1].strip()
+                body['pw'] = parts[2].strip()
+                passwords.append(body)
+                continue
+            elif not ',' in body['title']:
+                line = next(lines).strip().strip(':,')
+            else:
+                line = ', '.join(body['title'].split(',')[1:]).strip()
+                body['title']=body['title'].split(',')[0].strip()
+            if not ':' in line:
+                if ',' in line:
+                    parts = line.split(',')
+                    body['user'] = parts[0].strip()
+                    body['pw'] = parts[1].strip()
+                elif body['title'].lower().strip()=='bonuscard':
+                    parts = line.split(' ')
+                    body['user'] = parts[0].strip()
+                    body['pw'] = parts[1].strip()
+                else:
+                    if body['title'].lower().strip().find('alle')>0:
+                        body['user'] = 'alle'
+                        body['pw'] = line
+                    elif body['title'].lower().strip()=='miles&more':
+                        body['user'] = line
+                        body['pw'] = ""
+                    elif body['title'].lower().strip() in ['vtx','mieter verband','swisspass',
+                                                           'sbb','tuenti','helsana','axa',
+                                                           'travelcash','nzz','paypal',
+                                                           'postfinapp', 'upc connectbox',
+                                                           'helblingbl','tplink repeater']:
+                        body['user'] = ""
+                        body['pw'] = line
+                    else:
+                        body['user'] = line
+                        body['pw'] = next(lines)
+            else:
+                while ':' in line:
+                    parts = line.split(':')
+                    if parts[0].strip().lower() in ['username','user']:
+                        body['user'] = parts[1].strip()
+                    elif parts[0].strip().lower() in ['password','pw']:
+                        body['pw'] = parts[1].strip()
+                    else:
+                        body[parts[0].strip().lower()] = parts[1].strip()
+                    line = next(lines)
+                    lastline = line
+            passwords.append(body)
+        except StopIteration:
+            break
+
+    return passwords
+
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO)
+
+    files = glob.glob("input/*.html")
+
+    log.info("found {} notes!".format(len(files)))
+
+    now = datetime.now()
+
+    for file in files:
+        log.info(file)
+        with open(file, mode='r', encoding='utf-8') as fp:
+            soup = bs4.BeautifulSoup(fp.read().replace('\n','').replace('- ','* '), "html.parser")
+        googDate = parse(soup.select('.heading')[0].getText().strip())
+
+        # Get title
+        if len(soup.select('.title')) == 0:
+            title = ''
+        else:
+            title = soup.select('.title')[0].getText()
+
+        # selectors = {}
+        # for styles in soup.select('style'):
+        #     css = cssutils.parseString(styles.encode_contents())
+        #     for rule in css:
+        #         if rule.type == rule.STYLE_RULE:
+        #             style = rule.selectorText
+        #             selectors[style] = {}
+        #             for item in rule.style:
+        #                 propertyname = item.name
+        #                 value = item.value
+        #                 selectors[style][propertyname] = value
+
+        # Parse Content
+        body = soup.find("body")
+        note = next(body.children)
+
+        html = soup.select(".content")[0]
+
+
+        bullets = html.select(".bullet")
+        for bullet in bullets:
+            if bullet.get_text() == '☐':
+                bullet.replace_with("- [ ] ")
+            elif bullet.get_text() == '☑':
+                bullet.replace_with("- [x] ")
+
+
+        listitems = html.select(".listitem")
+        for listitem in listitems:
+            listitem.append('\n')
+            listitem.unwrap()
+
+
+        tags = soup.select(".chips")
+        if len(tags) > 0:
+            tags = tags[0]
+            tags = tags.select(".chip.label")
+            tags = [tag.get_text() for tag in tags]
+        else:
+            tags = []
+
+
+        # Convert linebreaks
+        for br in soup.find_all("br"):
+            br.replace_with("\n")
+
+        content = html.getText()
+
+        log.info("Title: {}".format(title))
+        log.info("Date: {}".format(googDate))
+        log.info("Tags: {}".format(tags))
+
+        if title.lower().strip() == 'logins':
+            passwords = parsePasswords(content)
+            for pw in passwords:
+                log.info("--> {}".format(pw))
+        else:
+            log.info("Content: \n{}".format(content))
